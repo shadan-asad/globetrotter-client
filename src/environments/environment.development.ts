b4cbb92f1484395ba12c8d85587b96d183aac593
Farhan's Guest <farhansguest@Farhans-MacBook-Air.local>
2025-03-02 12:09:34 +0530

    Added the dependencies for railway

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://jubilant-winner-4vq4559xprxc5qgw-5000.app.github.dev/api'
+  apiUrl: 'https://globetrotter-server-production.up.railway.app/api'
 }; 
